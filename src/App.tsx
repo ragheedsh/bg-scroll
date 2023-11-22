33bb0d0fc7777bdc90243f5fadcc4fe9b96b0db8
Ragheed <57329915+Ragheed350@users.noreply.github.com>
2023-11-22 16:53:11 +0400

    finish adding animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,80 @@
-import StickyBox from "react-sticky-box"
+import Lottie, { InteractivityProps } from "lottie-react"
 import { useEffect, useState } from "react"
 
-const imagesArr = [
-  "https://plus.unsplash.com/premium_photo-1699534955989-26a13746d060?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1700422300144-713dad3a1c4a?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1700427296131-0cc4c4610fc6?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1682686578842-00ba49b0a71a?q=80&w=1375&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1682687982423-295485af248a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1700159915592-004562ddcf6f?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-]
-const imageHeight = 600
+const firstInteractivity: Omit<InteractivityProps, "lottieObj"> | undefined = {
+  mode: "scroll",
+  actions: [
+    {
+      visibility: [0, 0.25],
+      type: "seek",
+      frames: [0],
+    },
+    {
+      visibility: [0.25, 0.45],
+      type: "seek",
+      frames: [0, 45],
+    },
+    {
+      visibility: [0.45, 1.0],
+      type: "seek",
+      frames: [45, 60],
+    },
+  ],
+}
+const secondInteractivity: Omit<InteractivityProps, "lottieObj"> | undefined = {
+  mode: "scroll",
+  actions: [
+    {
+      visibility: [0, 0.25],
+      type: "seek",
+      frames: [0],
+    },
+    {
+      visibility: [0.25, 0.45],
+      type: "seek",
+      frames: [0, 45],
+    },
+    {
+      visibility: [0.45, 1.0],
+      type: "seek",
+      frames: [45, 60],
+    },
+  ],
+}
 
 function App() {
-  const [scrollPosition, setScrollPosition] = useState(document.documentElement.scrollTop)
-  const [CurrentImageIndex, setCurrentImageIndex] = useState(0)
+  const [first, setfirst] = useState<any>()
+  const [second, setsecond] = useState<any>()
 
   useEffect(() => {
-    const handleScroll = () => {
-      setScrollPosition(document.documentElement.scrollTop)
-    }
-
-    window.addEventListener("scroll", handleScroll)
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll)
-    }
+    import("./animation/first-animation.json").then((file) => setfirst(file))
+    import("./animation/second-animation.json").then((file) => setsecond(file))
   }, [])
 
-  useEffect(() => {
-    setTimeout(() => {
-      const visibleImageIndex = Math.floor((scrollPosition + imageHeight / 2) / imageHeight)
-      console.log(visibleImageIndex)
+  if (!first || !second) {
+    return <>Loading...</>
+  } else
+    return (
+      <div>
+        <section style={{ height: 500, background: "black", color: "white" }}>Section 1</section>
 
-      visibleImageIndex < imagesArr.length && setCurrentImageIndex(visibleImageIndex)
-    })
-  }, [scrollPosition])
+        <section style={{ display: "flex", justifyContent: "center" }}>
+          <div style={{ width: "100%" }}>
+            <Lottie animationData={first} loop={false} interactivity={firstInteractivity} style={{ width: "100%", height: "100%" }} />
+          </div>
+        </section>
 
-  return (
-    <div>
-      <section style={{ minHeight: 500, border: "1px solid" }}>
-        <div>Section 1</div>
-      </section>
-      <section id="selected-section" style={{ minHeight: imagesArr.length * imageHeight }}>
-        <StickyBox offsetTop={0} offsetBottom={0}>
-          <div
-            className="image-style"
-            style={{
-              backgroundImage: `url(${imagesArr[CurrentImageIndex]})`,
-              height: imageHeight,
-            }}
-          />
-        </StickyBox>
-      </section>
-      <section style={{ minHeight: 500, border: "1px solid" }}>
-        <div>Section 3</div>
-      </section>
-    </div>
-  )
+        <section style={{ height: 500, background: "black", color: "white" }}>Section 2</section>
+
+        <section style={{ display: "flex", justifyContent: "center" }}>
+          <div style={{ width: "100%" }}>
+            <Lottie animationData={second} loop={false} interactivity={secondInteractivity} style={{ width: "100%", height: "100%" }} />
+          </div>
+        </section>
+
+        <section style={{ height: 500, background: "black", color: "white" }}>Section 3</section>
+      </div>
+    )
 }
 
 export default App
